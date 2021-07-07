e1e0e76438f3f5c5e52437aa1bc377fcdc524cb3
Paul Collantes <jeffersoncollantes31@gmail.com>
2021-07-06 21:03:42 -0500

    Contacto e Inicio

--- SISLimp/src/app/app.module.ts
+++ SISLimp/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './page/components/inicio/inicio.component';
+import { ContactoComponent } from './page/components/contacto/contacto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
