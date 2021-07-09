91db5dcbb9948a61f6e1b82cbb12be90c9e4d7c3
Jair <jair.quinonez@llacsaa.com>
2021-07-08 20:48:03 -0500

    Avance

---
+++ SISLimp/src/app/page/components/navigatebar/navigatebar.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navigatebar',
+  templateUrl: './navigatebar.component.html',
+  styleUrls: ['./navigatebar.component.css']
+})
+export class NavigatebarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+
+}
---
+++ SISLimp/src/app/page/components/producs-and-services/producs-and-services.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-producs-and-services',
+  templateUrl: './producs-and-services.component.html',
+  styleUrls: ['./producs-and-services.component.css']
+})
+export class ProducsAndServicesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
