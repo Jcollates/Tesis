87fb80f0335f6e3ecf413da944f376dd1fb827dc
Jair <jair.quinonez@llacsaa.com>
2021-07-21 00:00:49 -0500

    Cambios

--- SISLimp/src/app/page/components/products/products.component.ts
+++ SISLimp/src/app/page/components/products/products.component.ts
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
+  product: ProductEquleton[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.setProducts();
+  }
+  setProducts(){
+    this.product = [
+      {productName: "Cepillo", productDesq: "Compraeste cepillo o vete a mimir"},
+      {productName: "Ducha", productDesq: "Compraeste cepillo o vete a mimir"},
+      {productName: "Cartera", productDesq: "Compraeste cepillo o vete a mimir"},
+      {productName: "Radio", productDesq: "Compraeste cepillo o vete a mimir"},
+      {productName: "Cloro", productDesq: "Compraeste cepillo o vete a mimir"},
+      {productName: "Desinfectante", productDesq: "Compraeste cepillo o vete a mimir"},
+    ]
   }
 
+}
+
+export class ProductEquleton{
+  productName: string;
+  productDesq: string;
 }
